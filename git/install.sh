a8aff48148a1291a62b612f314ecfc45e04f37f4
Ludovic Werwinski <werwinski@lemonde.fr>
2016-01-30 18:01:48 +0100

    feat: add git config

---
+++ git/install.sh
@@ -0,0 +1,23 @@
+echo ""
+echo "[Git]"
+
+echo "››› Setup .gitconfig"
+
+GIT_CREDENTIAL="cache"
+
+if [ "$(uname -s)" == "Darwin" ]
+then
+  GIT_CREDENTIAL="osxkeychain"
+fi
+
+echo "- What is your github author name ?"
+read GIT_AUTHORNAME
+
+echo "- What is your github author email ?"
+read GIT_AUTHOREMAIL
+
+sed -e "s/AUTHORNAME/${GIT_AUTHORNAME}/g" -e "s/AUTHOREMAIL/${GIT_AUTHOREMAIL}/g" -e "s/GIT_CREDENTIAL_HELPER/${GIT_CREDENTIAL}/g" ./git/gitconfig.symlink.example > ./git/gitconfig.symlink
+
+echo "Creating gitconfig.symlink file"
+
+exit 0
