6c6f95581ccd125377e45f42ac2402162af930e0
Ludovic Werwinski <lwerwinski@gmail.com>
2017-06-25 19:16:30 +0200

    fix(zsh): bad substitution

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  git clone git://github.com/robbyrussell/oh-my-zsh.git "${$HOME}/.oh-my-zsh"
+  git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
 fi
