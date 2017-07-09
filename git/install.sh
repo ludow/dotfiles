ce0b621797cb73dc675fc96a0faca47fa1aaf8d8
Ludovic Werwinski <lwerwinski@gmail.com>
2017-07-09 03:20:54 +0200

    feat(git): check if gitconfig already exists

--- git/install.sh
+++ git/install.sh
@@ -3,21 +3,28 @@ echo "[Git]"
 
 echo "››› Setup .gitconfig"
 
-GIT_CREDENTIAL="cache"
+git_config () {
+  GIT_CREDENTIAL="cache"
 
-if [ "$(uname -s)" == "Darwin" ]
+  if [ "$(uname -s)" == "Darwin" ]
+  then
+    GIT_CREDENTIAL="osxkeychain"
+  fi
+
+  echo "- What is your github author name ?"
+  read GIT_AUTHORNAME
+
+  echo "- What is your github author email ?"
+  read GIT_AUTHOREMAIL
+
+  sed -e "s/AUTHORNAME/${GIT_AUTHORNAME}/g" -e "s/AUTHOREMAIL/${GIT_AUTHOREMAIL}/g" -e "s/GIT_CREDENTIAL_HELPER/${GIT_CREDENTIAL}/g" ./git/gitconfig.symlink.example > ./git/gitconfig.symlink
+
+  echo "Creating gitconfig.symlink file"
+}
+
+if [ ! -f "./git/gitconfig.symlink" ]
 then
-  GIT_CREDENTIAL="osxkeychain"
+  git_config
 fi
-
-echo "- What is your github author name ?"
-read GIT_AUTHORNAME
-
-echo "- What is your github author email ?"
-read GIT_AUTHOREMAIL
-
-sed -e "s/AUTHORNAME/${GIT_AUTHORNAME}/g" -e "s/AUTHOREMAIL/${GIT_AUTHOREMAIL}/g" -e "s/GIT_CREDENTIAL_HELPER/${GIT_CREDENTIAL}/g" ./git/gitconfig.symlink.example > ./git/gitconfig.symlink
-
-echo "Creating gitconfig.symlink file"
 
 exit 0
